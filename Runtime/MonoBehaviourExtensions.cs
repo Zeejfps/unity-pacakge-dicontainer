1305dddcc1310503e476e27fe25b4116c8635e87
Zee Vasilyev <zeejfps@gmail.com>
2025-11-07 13:59:22 -0600

    Added monobehaviour extensions

---
+++ Runtime/MonoBehaviourExtensions.cs
@@ -0,0 +1,32 @@
+using System.Reflection;
+using UnityEngine;
+
+namespace Vas.DependencyInjection
+{
+    public static class MonoBehaviourExtensions
+    {
+        public static void Inject(this MonoBehaviour monoBehaviour)
+        {
+            var properties = monoBehaviour
+                .GetType()
+                .GetProperties(BindingFlags.Public);
+
+            foreach (var property in properties)
+            {
+                var injected = property.GetCustomAttribute<InjectedAttribute>();
+                if (injected != null)
+                {
+                    var propertyType = property.PropertyType;
+                    if (DiContainer.Global.TryGet(propertyType, out var obj))
+                    {
+                        property.SetValue(monoBehaviour, obj);
+                    }
+                    else
+                    {
+                        Debug.LogWarning($"Failed to inject {property.Name}", monoBehaviour);
+                    }
+                }
+            }
+        }
+    }
+}
